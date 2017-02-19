68f614d58ee6b232b7ae0f678b1847f53ced8374
dvance <dvance@ucar.edu>
2017-02-18 18:04:52 -0700

    test for the system channel

---
+++ src/tests/SystemTest.php
@@ -0,0 +1,45 @@
+<?php
+
+class SystemTest extends TestCase
+{
+  protected $channel_id = 'system';
+
+  public function testChannel()
+  {
+    $this->endpointContainsJson('', [
+      'endpoint' => "/channel/{$this->channel_id}",
+      'type' => "channel",
+      'id' => $this->channel_id,
+      'title' => "Josheli TV",
+      'channel_id' => $this->channel_id
+    ]);
+
+    $json = $this->getJsonAsArray();
+
+    $this->assertArrayHasKey('background', $json);
+    $this->assertStringEndsWith('/channel/'.$this->channel_id.'/asset/background.jpg', $json['background']);
+    $this->assertArrayHasKey('thumb', $json);
+    $this->assertStringEndsWith('/channel/'.$this->channel_id.'/asset/thumb.jpg', $json['thumb']);
+
+    $this->assertArrayHasKey('items', $json);
+    $this->assertNotEmpty($json['items']);
+
+    $types = ['channel'];
+    $ids = ['christian-science'];
+
+    foreach($json['items'] as $item)
+    {
+      $this->assertArrayHasKey('type', $item);
+      $this->assertTrue(in_array($item['type'], $types));
+
+      if($item['type'] == 'track')
+      {
+        $this->assertEquals('mp3', $item['container']);
+      }
+
+      $this->assertArrayHasKey('id', $item);
+      $this->assertTrue(in_array($item['id'], $ids));
+
+    }
+  }
+}
