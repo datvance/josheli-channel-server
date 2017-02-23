4ad922ec145ce89dd605a34efefa3ce1ee606385
dvance <dvance@ucar.edu>
2017-02-22 20:53:23 -0700

    updated make:channel command to create a default MainMenu directory class

--- src/app/Console/Commands/MakeChannel.php
+++ src/app/Console/Commands/MakeChannel.php
@@ -50,7 +50,15 @@ class MakeChannel extends Command
       file_put_contents($new_directory . '/.gitkeep', '');
     }
     
-    file_put_contents($channel_path . '/' . $dir_name . '.php', $this->channelClassTemplate($dir_name, $given_name));
+    file_put_contents(
+      $channel_path . '/' . $dir_name . '.php',
+      $this->channelClassTemplate($dir_name, $given_name)
+    );
+
+    file_put_contents(
+      $channel_path . '/Directories/MainMenu.php',
+      $this->mainMenuClassTemplate($dir_name, $given_name)
+    );
 
     $this->info('New Channel, "' . $given_name . '", created at: ' . base_path('app/Channels/' . $dir_name));
     return true;
@@ -66,7 +74,29 @@ use App\Channels\Channel;
 
 class '.$channel_name.' extends Channel
 {
-  protected $description = \''.$given_name.'\';
+  protected $title = \''.$given_name.'\';
+}';
+  }
+
+  protected function mainMenuClassTemplate($channel_name, $given_name)
+  {
+    return '
+<?php
+
+namespace App\Channels\\'.$channel_name.'\Directories;
+
+use App\Channels\Directory;
+
+class MainMenu extends Directory
+{
+  protected $title = \''.$given_name.'\';
+  
+  public function items()
+  {
+    $items = [];
+    
+    return $items;
+  }
 }';
   }
 }
