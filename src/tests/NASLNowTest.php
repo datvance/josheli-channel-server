d907e0c5d7ff80e0fa6ade13d928010ea174d323
dvance <dvance@ucar.edu>
2017-02-26 21:27:19 -0700

    test for NASL Now

---
+++ src/tests/NASLNowTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Class NASLNowTest
+ *
+ * @group nasl-now
+ */
+class NASLNowTest extends TestCase
+{
+  protected $channel_id = 'nasl-now';
+
+  public function testChannel()
+  {
+    $this->endpointContainsJson('', [
+      'endpoint' => "/channel/{$this->channel_id}",
+      'type' => "channel",
+      'id' => $this->channel_id,
+      'title' => 'NASL Now',
+      'channel_id' => $this->channel_id
+    ]);
+
+    $json = $this->getJsonAsArray();
+
+    $this->assertArrayHasKey('background', $json);
+    $this->assertStringEndsWith('/channel/'.$this->channel_id.'/asset/background.jpg', $json['background']);
+    $this->assertArrayHasKey('thumb', $json);
+    $this->assertStringEndsWith('/channel/'.$this->channel_id.'/asset/thumb.jpg', $json['thumb']);
+
+    $this->assertArrayHasKey('items', $json);
+    $this->assertNotEmpty($json['items']);
+
+    $types = ['directory'];
+    $ids = ['official', 'latest'];
+
+    foreach($json['items'] as $item)
+    {
+      $this->assertArrayHasKey('type', $item);
+      $this->assertTrue(in_array($item['type'], $types));
+
+      $this->assertArrayHasKey('id', $item);
+      $this->assertTrue(in_array($item['id'], $ids));
+
+    }
+  }
+}
